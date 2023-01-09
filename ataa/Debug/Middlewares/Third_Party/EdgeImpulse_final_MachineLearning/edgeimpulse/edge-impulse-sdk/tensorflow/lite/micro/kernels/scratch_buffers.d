1bb4922b49a4e74a727388cdccf016d67034cbad
ataemir2101 <ata210190@gmail.com>
2023-01-09 21:01:49 +0300

    .
    Exams

---
+++ ataa/Debug/Middlewares/Third_Party/EdgeImpulse_final_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/EdgeImpulse_final_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_final_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/kernels/scratch_buffers.cpp \
+ c:\users\asus\desktop\ataa\middlewares\third_party\edgeimpulse_final_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h \
+ ../Middlewares/Third_Party/EdgeImpulse_final_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h
+c:\users\asus\desktop\ataa\middlewares\third_party\edgeimpulse_final_machinelearning\edgeimpulse\edge-impulse-sdk\classifier\ei_classifier_config.h:
+../Middlewares/Third_Party/EdgeImpulse_final_MachineLearning/edgeimpulse/tflite-model/trained_model_ops_define.h:
